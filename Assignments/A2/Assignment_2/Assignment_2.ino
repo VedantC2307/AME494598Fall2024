decfbe16edd0279ee87851eeed53838a64ccc1d5
Vedant <vedantchoudhary07@gmail.com>
2024-09-23 19:05:40 -0700

    Assignment 2 Complete!

--- Assignments/A2/Assignment_2/Assignment_2.ino
+++ Assignments/A2/Assignment_2/Assignment_2.ino
@@ -13,7 +13,20 @@ void setup()
     ttgo->tft->fillScreen(TFT_WHITE);
     ttgo->tft->setTextColor(TFT_BLACK, TFT_WHITE);
     ttgo->tft->setTextFont(4);
-    ttgo->tft->drawString("Vedant",  80, 118);
+
+    //Name
+    String name = "VEDANT";
+
+    // Calculate font height and width
+    int16_t nameWidth = ttgo->tft->textWidth(name);
+    int16_t nameHeight = ttgo->tft->fontHeight();
+
+    //Calculate x and y position of name
+    int16_t xPos = (ttgo->tft->width() - nameWidth) / 2;
+    int16_t yPos = (ttgo->tft->height() - nameHeight) / 2;
+
+    //Print name
+    ttgo->tft->drawString(name,  xPos, yPos);
 }
 
 void loop()
