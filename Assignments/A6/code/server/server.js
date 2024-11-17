f3ae34afef449c1663fb337baad22153864505ce
Vedant <vedantchoudhary07@gmail.com>
2024-11-17 14:36:55 -0700

    .

--- Assignments/A6/code/server/server.js
+++ Assignments/A6/code/server/server.js
@@ -35,14 +35,13 @@ app.get("/getAverage", function (req, res) {
   });
 });
 
-
 app.get("/getLatest", function (req, res) {
   (async function() {
     let client = await MongoClient.connect(connectionString,
       { useNewUrlParser: true });
     let db = client.db('sensorData');
     try {
-      let result = await db.collection("data").find().sort({time:-1}).limit(10).toArray();
+      let result = await db.collection("data").find({ time: { $gte: from, $lte: to } }).sort({time:-1}).limit(10).toArray();
       res.send(JSON.stringify(result));
     }
     finally {
